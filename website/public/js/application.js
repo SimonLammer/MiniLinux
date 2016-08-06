8dfe580636964a0d7e79f59f8f55bf47e49417c7
Simon Lammer <lammer.simon@gmail.com>
2016-08-06 18:09:45 +0200

    Fix js-link

--- website/public/js/application.js
+++ website/public/js/application.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function() {
 			$('.content').html($(data).find('.content')); 	// Pull the post we want out of the .content class.
 															// If you change the class of the post container,
 															// you must change it here!!!
-			_gaq.push(['_trackPageview', State.url]);	// This updates Google Analytics with a visit to the new page.
+			//_gaq.push(['_trackPageview', State.url]);	// This updates Google Analytics with a visit to the new page.
 														// If you don't use Google Analytics, you can safety comment or
 														// remove that line.
 		});
